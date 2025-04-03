d6be807c85cb15900dde9f615a607d0c0ab97e95
nereazj544 <nzjvirtual@gmail.com>
2025-04-03 21:34:43 +0200

    Agregar ejemplos de uso de variables y tipos de datos en HelloWorld.kt y configurar opciones de comentarios en betterCommentsSettings.xml

--- Kotlin/ESTUDIO1/src/main/kotlin/HelloWorld.kt
+++ Kotlin/ESTUDIO1/src/main/kotlin/HelloWorld.kt
@@ -1,3 +1,37 @@
 fun main() {
+    //TODO Mostrar mensjaes por pantalla
     println("Hola Koltin");
+
+    // TODO Sintaxis
+
+//? Variables
+    var myString = "Esto es una cadena de texto";
+    println(myString);
+    myString = "Cambio de cadena";
+    println(myString);
+
+    var myString2: String = "esto es con tipacion";
+
+    var myInt = 9; //numeros interos
+    myInt = myInt -3;
+    println(myInt);
+    println(myInt + 4);
+
+
+    var myDouble = 3.4; //? Numeros decimales
+    myDouble = 9.0
+
+    //! EVITAR TIPAR PERO ES PARA VER EL TIPO DE DATO VIENE BIEN
+    var myFloat: Float = 5.2f
+    myFloat - 4f
+
+    var myBoolean = true //? Esto solo falso o verdadero
+    
+
+
+
+
+
+
+
 }
