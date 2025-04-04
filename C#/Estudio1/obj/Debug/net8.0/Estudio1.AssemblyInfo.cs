5ae20b40df8258c08d31390ef400875e80fabfe7
nereazj544 <nzjvirtual@gmail.com>
2025-04-04 16:13:55 +0200

    Actualizar archivos de proyecto: modificar versiones de ensamblado y agregar archivo helloworld.swift con ejemplos de uso de variables y constantes; crear README.md vacío

--- C#/Estudio1/obj/Debug/net8.0/Estudio1.AssemblyInfo.cs
+++ C#/Estudio1/obj/Debug/net8.0/Estudio1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Estudio1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2810cffc43dfa5c1c1ce4a37075326025bd1844b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fdf66d90f552b78158ae211fa70c76949711d1a5")]
 [assembly: System.Reflection.AssemblyProductAttribute("Estudio1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Estudio1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ Swift/ESTUDIO/helloworld.swift
@@ -0,0 +1,22 @@
+// Librerías
+import Foundation  // Core de Swift
+
+// No es necesario importar UIKit si no estás desarrollando UI
+// import UIKit    // Solo necesario para apps iOS/macOS con interfaz gráfica
+
+// Variables
+print("Hello World")
+var name = "Neo"
+var age = 20
+var myString = "This is a string (characters text)"
+var myInt = 23
+var myDouble = 23.5
+var myFloat: Float = 23.5  // Tipado explícito para Float
+var myBool = false
+
+// Valor nulo
+var myNil: String? = nil  // Variable opcional
+
+// Constantes
+let myConst = "Mi propiedad constante"
+print(myConst)
